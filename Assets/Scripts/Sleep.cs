168a92bd5a16d878e83d01272265bf1d9371b0bc
Ricci-Liu <rl4800@nyu.edu>
2024-04-15 01:50:18 +0800

    sleep脚本

--- Assets/Scripts/Sleep.cs
+++ Assets/Scripts/Sleep.cs
@@ -1,18 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Sleep : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Camera mainCamera;  // 主摄像机
+    public Camera sleepCamera;  // 第二摄像机
+    public float transitionSpeed = 1.0f;  // 过渡速度
+    public float transitionThreshold = 0.05f;  // 过渡阈值
+
+    private Transform _mainCameraPos;
+    private bool isSwitching = false;
+    private bool _ePressed = false;
+    private Vector3 _targetPosition;
+    private Quaternion _targetRotation;
+
+    private void OnEnable()
     {
-        
+        sleepCamera.enabled = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E) && !_ePressed)
+        {
+            _ePressed = true;
+            isSwitching = true;
+            mainCamera.enabled = false;
+            sleepCamera.enabled = true;
+            _mainCameraPos = mainCamera.transform;
+            _targetPosition = _mainCameraPos.position;
+            _targetRotation = _mainCameraPos.rotation;
+        }
+
+        if (isSwitching)
+        {
+            SwitchCamera();
+        }
+    }
+
+    void SwitchCamera()
+    {
+        // 平滑过渡效果
+        sleepCamera.transform.position = Vector3.Lerp(sleepCamera.transform.position, _targetPosition, transitionSpeed * Time.deltaTime);
+        sleepCamera.transform.rotation = Quaternion.Lerp(sleepCamera.transform.rotation, _targetRotation, transitionSpeed * Time.deltaTime);
+
+        // 检查是否接近目标位置和旋转
+        if (Vector3.Distance(sleepCamera.transform.position, _targetPosition) < transitionThreshold &&
+            Quaternion.Angle(sleepCamera.transform.rotation, _targetRotation) < transitionThreshold)
+        {
+            // 停止过渡
+            isSwitching = false;
+        }
     }
 }
