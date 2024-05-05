0c3d5d1a99be761bb6c0c7ef77cd205bc3a4ab5a
abbre <sc9771@nyu.edu>
2024-05-06 00:22:15 +0800

    day2

--- Assets/Scripts/Day2/Eating.cs
+++ Assets/Scripts/Day2/Eating.cs
@@ -2,28 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Eating : MonoBehaviour
+public class PlayerInteraction2 : MonoBehaviour
 {
-    public List<GameObject> foodObjects = new List<GameObject>(); // 食物对象的列表
     public GameObject interactionIcon; // 交互图标
     public float interactionDistance = 3f; // 相机检测距离
 
+    public Camera mainCamera;
+    private Collider _collider;
     private bool _Eshowed = false;
+
+    public List<AudioClip> eatingSounds;
+    private AudioSource audioSource;
+
+    private List<GameObject> objects; // 存储盘子里的水果
 
     void Start()
     {
+        _collider = GetComponent<Collider>();
+
         interactionIcon.SetActive(false); // 初始隐藏交互图标
+        mainCamera = GameManager.Camera;
+    
+        audioSource = GetComponent<AudioSource>();
+
+        // 初始化水果列表
+        objects = new List<GameObject>();
+        foreach (Transform child in transform)
+        {
+           objects.Add(child.gameObject);
+        }
     }
 
     void Update()
     {
         RaycastHit hit;
-        Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)); // 从屏幕中心发出射线
+        Ray ray = mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)); // 从屏幕中心发出射线
 
         if (Physics.Raycast(ray, out hit, interactionDistance))
         {
             // 如果射线击中了可以交互的物体
-            if (foodObjects.Contains(hit.collider.gameObject))
+            if (hit.collider == _collider)
             {
                 if (!_Eshowed)
                 {
@@ -33,46 +51,43 @@ public class Eating : MonoBehaviour
 
                 if (Input.GetKeyDown(KeyCode.E))
                 {
-                    EatRandomFood();
+                    EatFruit();
+                    _Eshowed = false;
                 }
             }
             else
             {
-                // 如果射线击中的不是食物对象，隐藏交互图标
-                HideInteractionIcon();
+                // 如果射线没有击中任何物体，隐藏交互图标
+                interactionIcon.SetActive(false);
+                _Eshowed = false;
             }
         }
-        else
-        {
-            // 如果射线没有击中任何物体，隐藏交互图标
-            HideInteractionIcon();
-        }
     }
 
-    void HideInteractionIcon()
+    void EatFruit()
     {
-        interactionIcon.SetActive(false);
-        _Eshowed = false;
-    }
-
-    void EatRandomFood()
-    {
-        if (foodObjects.Count > 0)
+        // 如果还有水果
+        if (objects.Count > 0)
         {
-            // 随机选择一个食物对象
-            int randomIndex = Random.Range(0, foodObjects.Count);
-            GameObject food = foodObjects[randomIndex];
+            // 随机选择一个水果
+            int randomIndex = Random.Range(0, objects.Count);
+            GameObject fruitToEat = objects[randomIndex];
 
-            // 禁用选中的食物对象
-            food.SetActive(false);
+            // 禁用选中的水果
+            fruitToEat.SetActive(false);
+            objects.RemoveAt(randomIndex);
 
-            // 从食物列表中移除被禁用的食物对象
-            foodObjects.RemoveAt(randomIndex);
-
-            // 检查食物列表是否为空，如果为空，隐藏交互图标
-            if (foodObjects.Count == 0)
+            // 播放随机的吃东西声音
+            if (eatingSounds.Count > 0 && audioSource != null)
             {
-                HideInteractionIcon();
+                int randomSoundIndex = Random.Range(0, eatingSounds.Count);
+                audioSource.clip = eatingSounds[randomSoundIndex];
+                audioSource.Play();
+            }
+
+            if (objects.Count == 0)
+            {
+                interactionIcon.SetActive(false);
             }
         }
     }
